da25849d99c0bbaf835a7a656e6d789c5bf8f640
tianyahuanxiang <2504_92365747@noreply.gitcode.com>
2026-01-05 11:21:30 +0800

    加入设备操作函数

--- register_unregister.c
+++ register_unregister.c
@@ -8,7 +8,8 @@ static int __init xxx_init(void)
     int retvalue = 0;
 
     /* 注册字符设备驱动 */
-    retvalue = register_chrdev(200, "chrtest", &test_fops); // 200为主设备号, "chrtest"为设备名, &test_fops为文件操作集
+    retvalue = register_chrdev(200, "chrtest", &test_fops); 
+    // 200为主设备号, "chrtest"为设备名, &test_fops为文件操作集
     if(retvalue < 0)
     {
         /* 注册失败 */
@@ -21,7 +22,8 @@ static int __init xxx_init(void)
 static void __exit xxx_exit(void)
 {
     /* 注销字符设备驱动 */
-    unregister_chrdev(200, "chrtest"); // 200为主设备号, "chrtest"为设备名
+    unregister_chrdev(200, "chrtest"); 
+    // 200为主设备号, "chrtest"为设备名
 }
 
 // 将上面两个函数指定为驱动的入口和出口函数
